c0cc2b98def3c197049b307005d41c61a451aafc
Aslihan <aslihanasadova@gmail.com>
2020-03-04 13:25:32 -0500

    fixed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import Dogs from './Dogs'
-import Cats from '.Cats'
+import Cats from './Cats'
 import Adoptable from './Adoptable'
+import PetTypesContainer from './PetTypesContainer'
 
 
 const NavBar = props =>{
     return(
         <div>
             <h3>Hello</h3>
-            <Link to="/dogs">DOGS</Link>
+            <ul>
+            <Link to="/dogs">List Pets</Link>
             <Link to="/cats">CATS</Link>
             <Link to="/home">HOME</Link>
             <Link to="/adoptable">ADOPT ME!</Link>
+            </ul>
+       
         <Switch>
+            <Route exact path="/" component={PetTypesContainer}/>   
             <Route exact path="/dogs" component={Dogs}/>
             <Route exact path="/cats" component={Cats}/>
-            <Route exact path="/home" component={Home}/>
             <Route exact path="/adoptable" component={Adoptable}/>
         </Switch>
         </div>
