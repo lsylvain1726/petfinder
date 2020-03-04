b35643696788b453cd52967a53dce867af242e5d
Aslihan <aslihanasadova@gmail.com>
2020-03-04 17:33:57 -0500

    App.js and NavBar updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,6 @@
 import React from "react"
 import { Route, Switch, BrowserRouter } from "react-router-dom"
 import NavBar from "./NavBar"
-import ListPets from './ListPets'
-import PetTypesContainer from "./PetTypesContainer"
-import PetShowContainer from "./PetShowContainer"
-import NotFound from "./NotFound"
 
 const App = props => {
   return (
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Link, Switch} from 'react-router-dom'
+import { Route, Link, Switch, BrowserRouter} from 'react-router-dom'
 import PetTypesContainer from './PetTypesContainer'
 import PetShowContainer from './PetShowContainer'
 import ListPets from './ListPets'
@@ -9,29 +9,30 @@ import NotFound from './NotFound'
 
 const NavBar = props =>{
     return(
-        <div>
-            <h1>HELLO TESTING HERE</h1>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">HOME</Link>
-                    </li>
-
+        <BrowserRouter>
+            <div>
+                <h1>HELLO TESTING HERE</h1>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/pets">HOME</Link>
+                        </li>
                         <li>
                             <Link to="/pets/dog">DOGS</Link>
                         </li>
-                    <li>
-                        <Link to="/pets/cat">CATS</Link>
-                    </li>
-                </ul>
-        </nav>
-        <Switch>
-            <Route exact path="/pets" component={ListPets} />
-            <Route exact path="/pets/:animalType/:id" component={PetShowContainer} />
-            <Route exact path="/pets/:animalType" component={PetTypesContainer} />
-            <Route path="*" component={NotFound} />
-        </Switch>
-        </div>
+                        <li>
+                            <Link to="/pets/cat">CATS</Link>
+                        </li>
+                    </ul>
+            </nav>
+            <Switch>
+                <Route exact path="/pets" component={ListPets} />
+                <Route exact path="/pets/:animalType" component={PetTypesContainer} />
+                <Route exact path="/pets/:animalType/:id" component={PetShowContainer} />
+                <Route path="*" component={NotFound} />
+            </Switch>
+            </div>
+        </BrowserRouter>
            
     )
 }
