0f2e505470997c408e250e213819b7ac0ced5de6
Aslihan <aslihanasadova@gmail.com>
2020-03-04 15:01:05 -0500

    routes order fixed

--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,8 @@ const App = props => {
       <Switch>
         <Route exact path="/pets" component={ListPets} />
         <Route exact path="/pets/:animalType/:id" component={PetShowContainer} />
-        <Route path="*" component={NotFound} />
         <Route exact path="/pets/:animalType" component={PetTypesContainer} />
+        <Route path="*" component={NotFound} />
       </Switch>
     </BrowserRouter>
   )
